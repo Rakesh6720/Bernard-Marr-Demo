e3856281614da2f578e3e63ca4c57f6c3e5c13b8
Rakesh Baruah <rbaruah@gmail.com>
2023-04-21 11:06:17 -0500

    change hero image to grayscale

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
         </button>
       </div>
       <Image
-        className="hidden lg:block lg:w-3/4"
+        className="hidden lg:block lg:w-3/4 grayscale"
         src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
         alt="Business man talking"
         width={700}
