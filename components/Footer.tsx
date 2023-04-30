3ca8014deac3a49610048c20a98e8010f0a1ebe5
Rakesh Baruah <rbaruah@gmail.com>
2023-04-30 14:28:14 -0500

    adjust footer css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between border-t-2 border-black mt-5 flex-col md:flex-row">
-      <div className="py-5 px-5">
+    <div className="flex justify-between border-t-2 border-black mt-5 md:flex-row flex-col-reverse">
+      <div className="md:py-5 px-5 sm:py-2">
         <div className="flex-col ">
           <div className="py-3">
             &copy; Bernard Marr {new Date().getFullYear()}
@@ -14,11 +14,11 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="py-5 px-5">
-        <div className="py-3">Terms & Conditions</div>
-        <div className="py-3">Privacy Policy</div>
+      <div className="md:py-5 px-5 sm:py-2">
+        <div className="md:py-3 sm:py-0">Terms & Conditions</div>
+        <div className="md:py-3 sm:py-0">Privacy Policy</div>
       </div>
-      <div className="py-10 pl-5">
+      <div className="md:py-10 pl-5 py-7">
         <h1 className="text-black w-80 text-2xl">Bernard Marr & Co.</h1>
         <h3 className="text-md leading-3 text-black">
           Future . Business . Success
