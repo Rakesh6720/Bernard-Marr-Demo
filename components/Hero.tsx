545542451ad452ce5be56f2932828d0341d8b845
Rakesh Baruah <rbaruah@gmail.com>
2023-04-30 12:56:27 -0500

    reset hero modal styles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,15 +3,16 @@ import HeroModal from "./HeroModal";
 
 export default function Hero() {
   return (
-    <div className="flex">
-      <HeroModal />
+    <div className="lg:flex-row-reverse lg:flex sm:flex sm:flex-col">
       <Image
-        className="hidden lg:block lg:w-3/4 grayscale"
+        className="grayscale lg:w-3/4 sm:w-full"
         src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
         alt="Business man talking"
         width={700}
         height={100}
       />
+
+      <HeroModal />
     </div>
   );
 }
--- components/HeroModal.tsx
+++ components/HeroModal.tsx
@@ -42,23 +42,21 @@ const HeroModal = () => {
     }
   };
   return (
-    <>
-      <div className="lg:z-10 bg-[#c6ce00] lg:w-3/4 lg:-mr-20 p-10 lg:my-auto lg:flex-col lg:justify-center">
-        <h3 className="text-white text-5xl py-5">{data[index].title}</h3>
-        <p className="text-white text-lg">{data[index].text}</p>
-        <button className="bg-black text-white px-8 py-3 mt-5 mb-5">
-          <Link href="/services">VIEW MORE</Link>
+    <div className="bg-[#c6ce00] z-10 lg:h-1/2 lg:my-auto lg:ml-20 lg:-mr-20 lg:p-10 sm:p-10 sm:w-1/2 sm:mx-auto sm:-mt-20 sm:mb-10">
+      <h3 className="text-white text-4xl py-5">{data[index].title}</h3>
+      <p className="text-white text-md">{data[index].text}</p>
+      <button className="bg-black text-white px-8 py-3 mt-5 mb-5">
+        <Link href="/services">VIEW MORE</Link>
+      </button>
+      <div className="flex bg-black h-8 w-full justify-between -mb-10">
+        <button className="text-white px-10" onClick={onLeftClickHandler}>
+          <IoIosArrowBack />
         </button>
-        <div className="flex bg-black h-8 w-full justify-between -mb-10">
-          <button className="text-white px-10" onClick={onLeftClickHandler}>
-            <IoIosArrowBack />
-          </button>
-          <button className="text-white px-10" onClick={onRightClickHandler}>
-            <IoIosArrowForward />
-          </button>
-        </div>
+        <button className="text-white px-10" onClick={onRightClickHandler}>
+          <IoIosArrowForward />
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
