b5b0fe05daa409d912f738e72c510fb38cad1201
Rakesh Baruah <rbaruah@gmail.com>
2023-05-02 09:39:13 -0500

    change hero modal color background

--- components/HeroModal.tsx
+++ components/HeroModal.tsx
@@ -2,14 +2,13 @@
 
 import Link from "next/link";
 import React, { useState } from "react";
-import { AiOutlineArrowLeft } from "@react-icons/all-files/ai/AiOutlineArrowLeft";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 const data = [
   {
     id: 1,
     title: "Influencer",
-    text: "Amit Manghani is one of the world&apos;s most successful social media influencers at the intersection of business and technology",
+    text: "Amit Manghani is one of the world's most successful social media influencers at the intersection of business and technology",
   },
   {
     id: 2,
@@ -43,7 +42,7 @@ const HeroModal = () => {
   };
   return (
     <div className="lg:bg-none bg-[url('https://images.unsplash.com/photo-1507878866276-a947ef722fee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80')] bg-cover relative lg:my-auto">
-      <div className="bg-[#c6ce00] pb-10 px-5 w-3/4 mx-auto z-10 top-11 sm:relative lg:w-[700px] lg:h-[300px] lg:-mr-20 sm:ml-24 relative">
+      <div className="bg-slate-700 pb-10 px-5 w-3/4 mx-auto z-10 top-11 sm:relative lg:w-[700px] lg:h-[300px] lg:-mr-20 sm:ml-24 relative">
         <h3 className="text-white text-4xl py-5">{data[index].title}</h3>
         <p className="text-white text-md">{data[index].text}</p>
         <button className="bg-black text-white px-8 py-3 mt-5 mb-5">
